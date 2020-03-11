85fb1512b855095781c4faa6537ca883f3986932
pierre <pierre@pc.lan>
2020-03-11 21:40:13 +0100

    l

--- includes/durex.h
+++ includes/durex.h
@@ -1,6 +1,10 @@
 #ifndef _DUREX_H_
 # define _DUREX_H_
 
+# include <sys/types.h>
+# include <sys/stat.h>
+
 # include <unistd.h>
+# include <elf.h>
 
 #endif
--- srcs_durex/main.c
+++ srcs_durex/main.c
@@ -1,7 +1,25 @@
 #include "durex.h"
 
-int main(void)
+#include <stdio.h>
+
+int get_info(struct stat *st, char *path)
 {
-	write(STDIN_FILENO, "pnardozi\n", 9); 
+
+}
+
+int main(int argc, char **argv)
+{
+	struct stat	st;
+	int		in_durex;
+
+	(void)argc;
+	in_durex = 1;
+	if (in_durex)
+	{
+		write(STDIN_FILENO, "pnardozi\n", 9); 
+	}
+	else
+	{
+	}
 	return (0);
 }
